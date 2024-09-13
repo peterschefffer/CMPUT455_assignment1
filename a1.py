d3ca3fdedf267b8aaaf9679037a4e7b43106549e
dannyboy777257 <danielvovk0@gmail.com>
2024-09-13 14:04:16 -0600

    genmove picks from rand in list of lists made by check moves. Winner still incorrect

--- a1.py
+++ a1.py
@@ -83,27 +83,54 @@ class CommandInterface:
         #raise NotImplementedError("This command is not yet implemented.")
         if self.board:
             for point in self.board:
-                print(" ".join([str(x) for x in point]))
+                print("".join([str(x) for x in point]))
         return True
     
     def play(self, args):
         #raise NotImplementedError("This command is not yet implemented.")
         
-        legal = self.legal(args)
+        legal = self.legal(args, silent=True)
 
-        # check if legal
-        if not legal:
+        # # check if legal
+        # if not legal:
+        #     return False
+
+        formatted_arg = ' '.join([arg if arg.isdigit() else f"{arg}" for arg in args])
+
+        # check for correct number of arguments
+        if legal == -3:
+            print(f"= illegal move: {formatted_arg} wrong number of arguments")
             return False
 
+        # check that play position is in bounds
+        if legal == -4:
+            print(f"= illegal move: {formatted_arg} wrong coordinate")
+            return False
+        
+        # if x_pos < 0 or x_pos > self.x_dim - 1 or y_pos < 0 or y_pos > self.y_dim - 1:
+        #     print(f"= illegal move: {formatted_arg} wrong coordinate")
+        #     return False
+        
+        # check that digit is in {0,1}
+        if legal == -5:
+            print(f"= illegal move: {formatted_arg} wrong number")
+            return False
+        
+        #check that board space is not occupied
+        # current_board_space = self.board[y_pos][x_pos]
+        if legal == -6:
+            print(f"= illegal move: {formatted_arg} occupied")
+            return False
+            
         x_pos = int(args[0])
         y_pos = int(args[1])
         digit = int(args[2])
-            
+
         if legal == 1:
             self.board[y_pos][x_pos] = digit
             self.change_player()
         elif legal == -1:
-            print(f"= illegal move: {x_pos} {y_pos} {digit} three in a row violation")
+            print(f"= illegal move: {x_pos} {y_pos} {digit} three in a row")
             return False
         elif legal == -2:
             print(f"= illegal move: {x_pos} {y_pos} {digit} too many {digit}")
@@ -115,52 +142,78 @@ class CommandInterface:
     Return -1 for a violation of 3 in a row
     Return -2 for a balance violationc  
     Return 1 for legal move
+    Return -3 for wrong number of arguments
+    Return -4 for wrong coordinate
+    Return -5 for wrong number
+    Return -6 for occupied
 
     TODO implement 3 in a row check
     '''
-    def legal(self, args):
+    def legal(self, args, silent = False):
 
-        formatted_arg = ' '.join([arg if arg.isdigit() else f"'{arg}'" for arg in args])
+        # formatted_arg = ' '.join([arg if arg.isdigit() else f"{arg}" for arg in args])
 
         # check for correct number of arguments
         if len(args) != 3:
-            print(f"= illegal move: {formatted_arg} wrong number of arguments")
-            return False
+            # print(f"= illegal move: {formatted_arg} wrong number of arguments")
+            if not silent:
+                print("no")
+            return -3
 
         # check that play position is in bounds
-        if any(not arg.isdigit() for arg in args):
-            print(f"= illegal move: {formatted_arg} wrong coordinate")
-            return False
+        if any(not arg.isdigit() for arg in args[:2]):
+            if not silent:
+                print("no")
+            # print(f"= illegal move: {formatted_arg} wrong coordinate")
+            return -4
         
         x_pos = int(args[0])
         y_pos = int(args[1])
-        digit = int(args[2])
         
         if x_pos < 0 or x_pos > self.x_dim - 1 or y_pos < 0 or y_pos > self.y_dim - 1:
-            print(f"= illegal move: {formatted_arg} wrong coordinate")
-            return False
+            # print(f"= illegal move: {formatted_arg} wrong coordinate")
+            if not silent:
+                print("no")
+            return -4
         
+        if not args[2].isdigit():
+            if not silent:
+                print("no")
+            return -5
+        
+        digit = int(args[2])
+
         # check that digit is in {0,1}
         if not digit in [0,1]:
-            print(f"= illegal move: {formatted_arg} wrong number")
-            return False
+            # print(f"= illegal move: {formatted_arg} wrong number")
+            if not silent:
+                print("no")
+            return -5
         
         #check that board space is not occupied
         current_board_space = self.board[y_pos][x_pos]
         if current_board_space != ".":
-            print(f"= illegal move: {formatted_arg} occupied")
-            return False
+            # print(f"= illegal move: {formatted_arg} occupied")
+            if not silent:
+                print("no")
+            return -6
         
         # 3 in a row X direction violation
         if self.x_dim >= 3:
             if x_pos >= 2:
                 if self.board[y_pos][x_pos - 1] == digit and self.board[y_pos][x_pos - 2] == digit:
+                    if not silent:
+                        print("no")
                     return -1
             if x_pos <= self.x_dim - 3:
                 if self.board[y_pos][x_pos + 1] == digit and self.board[y_pos][x_pos + 2] == digit:
+                    if not silent:
+                        print("no")
                     return -1
             if x_pos >= 1 and x_pos < self.x_dim - 1:
                 if self.board[y_pos][x_pos - 1] == digit and self.board[y_pos][x_pos + 1] == digit:
+                    if not silent:
+                        print("no")
                     return -1
             # if x_pos <= self.x_dim - 2:
             #     if self.board[y_pos][x_pos - 1] == digit and self.board[y_pos][x_pos + 1] == digit:
@@ -170,12 +223,18 @@ class CommandInterface:
         if self.y_dim >= 3:
             if y_pos >= 2:
                 if self.board[y_pos - 1][x_pos] == digit and self.board[y_pos - 2][x_pos] == digit:
+                    if not silent:
+                        print("no")
                     return -1
             if y_pos <= self.y_dim - 3:
                 if self.board[y_pos + 1][x_pos] == digit and self.board[y_pos + 2][x_pos] == digit:
+                    if not silent:
+                        print("no")
                     return -1
             if y_pos >= 1 and y_pos < self.y_dim - 1:
                 if self.board[y_pos - 1][x_pos] == digit and self.board[y_pos + 1][x_pos] == digit:
+                    if not silent:
+                        print("no")
                     return -1
             # if y_pos <= self.y_dim - 2:
             #     if self.board[y_pos - 1][x_pos] == digit and self.board[y_pos + 1][x_pos] == digit:
@@ -193,6 +252,8 @@ class CommandInterface:
                 x_count += 1       
 
         if x_count == x_max:
+            if not silent:
+                print("no")
             return -2
         
         for i in range(self.y_dim):
@@ -200,8 +261,12 @@ class CommandInterface:
                 y_count += 1
 
         if y_count == y_max:
+            if not silent:
+                print("no")
             return -2
         
+        if not silent:
+            print("yes")
         return 1
     
     def genmove(self, args):
@@ -210,47 +275,54 @@ class CommandInterface:
 
         
         #text needs to be added
+        legal_moves = self.check_moves()
+        # status  =  self.check_moves()
 
-        status  =  self.check_moves()
-
-        if status == True:
+        if not legal_moves:
             print('resign')
             return True
 
+        rand_move = random.choice(legal_moves)
+        x_pos = rand_move[0]
+        y_pos = rand_move[1]
+        digit = rand_move[2]
 
-        args = []
+        self.board[y_pos][x_pos] = digit
+        self.change_player()
+        print(x_pos,y_pos,digit)
+        # args = []
 
-        x_pos = random.randint(0,self.x_dim-1)
-        args.append(str(x_pos))
-        y_pos = random.randint(0, self.y_dim-1)
-        args.append(str(y_pos))
-        digit = random.randint(0,1)
-        args.append(str(digit))
+        # x_pos = random.randint(0,self.x_dim-1)
+        # args.append(str(x_pos))
+        # y_pos = random.randint(0, self.y_dim-1)
+        # args.append(str(y_pos))
+        # digit = random.randint(0,1)
+        # args.append(str(digit))
 
 
         # syntax options when testing
     
 
-        print(x_pos,y_pos,digit)
+        # print(x_pos,y_pos,digit)
         #print(f"@{x_pos} {y_pos} {digit}")
         
         
-        legal = self.legal(args)
+        # legal = self.legal(args, silent=True)
 
-        # check if legal
-        if not legal:
-            return False
+        # # check if legal
+        # if not legal:
+        #     return False
             
-        if legal == 1:
-            self.board[y_pos][x_pos] = digit
-            self.change_player()
+        # if legal == 1:
+        #     self.board[y_pos][x_pos] = digit
+        #     self.change_player()
 
-        elif legal == -1:
-            print(f"= illegal move: {x_pos} {y_pos} {digit} three in a row violation")
-            return False
-        elif legal == -2:
-            print(f"= illegal move: {x_pos} {y_pos} {digit} too many {digit}")
-            return False
+        # elif legal == -1:
+        #     print(f"= illegal move: {x_pos} {y_pos} {digit} three in a row")
+        #     return False
+        # elif legal == -2:
+        #     print(f"= illegal move: {x_pos} {y_pos} {digit} too many {digit}")
+        #     return False
          
         return True
 
@@ -259,26 +331,36 @@ class CommandInterface:
 
     def check_moves(self):
         
-        args = []
+        legal_moves = []
 
         for y in range(self.y_dim):
              for x in range(self.x_dim):
                 pos =  self.board[y][x]
                 if pos == "." :
-                    args.clear()
-                    args.append(str(x))
-                    args.append(str(y)) 
-                    args.append("0")
-                    legal = self.legal(args)
-                    if legal == 1 :
-                        return False
-                    args.pop()
-                    args.append("1")
-                    legal = self.legal(args)
-                    if legal == 1 : 
-                        return False
+                    check0 = [str(x),str(y),"0"]
+                    if self.legal(check0, silent=True) == 1:
+                        legal_moves.append([x, y, 0])
 
-        return True
+                    check1 = [str(x),str(y),"1"]
+                    if self.legal(check1, silent=True) == 1:
+                        legal_moves.append([x, y, 1])
+                # if pos == "." :
+                #     args.clear()
+                #     args.append(str(x))
+                #     args.append(str(y)) 
+                #     args.append("0")
+                #     legal = self.legal(args)
+                #     if legal == 1 :
+                #         return False
+                #     args.pop()
+                #     args.append("1")
+                #     legal = self.legal(args)
+                #     if legal == 1 : 
+                #         return False
+
+
+        return legal_moves
+        # return True
 
 
     
